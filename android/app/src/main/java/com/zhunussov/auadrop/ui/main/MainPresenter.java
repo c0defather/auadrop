45c379102cc5634c1ea394d635d29130fd0117db
Kuanysh Zhunussov <kuanysh@nobleapplications.com>
2018-10-08 18:10:42 +0600

    update gitignore

--- android/app/src/main/java/com/zhunussov/auadrop/ui/main/MainPresenter.java
+++ android/app/src/main/java/com/zhunussov/auadrop/ui/main/MainPresenter.java
@@ -100,7 +100,7 @@ public class MainPresenter extends MvpPresenter<MainContract.View> implements Ma
                     @Override
                     public void onComplete() {
                         getViewState().hideProgress();
-                        router.navigateTo("sdf");
+                        router.exit();
                     }
                 });
     }
